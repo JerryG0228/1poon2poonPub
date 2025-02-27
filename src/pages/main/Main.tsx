f2d4fd9bcc5756fc4c5c7b1cc14c25e1fbc7297f
ooinl77 <ooinl77@naver.com>
2025-02-27 16:23:22 +0900

    Feat: PointBox 컴포넌트 추가 Rename: assets 하위 폴더 이름 변경 Feat: StampBox 이미지 추가

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,15 +1,13 @@
-import StampBox from '@/components/StampBox';
-import BusIcon from '@/assets/main/BusIcon.png';
-import { useState } from 'react';
+import PointBox from '@/components/PointBox';
 
 export default function Main() {
-  const [title, setTitle] = useState<string>('대중교통');
-  const [img, setImg] = useState<string>(BusIcon);
-  const [bgColor, setBgColor] = useState<string>('#6e5230');
   return (
-    <div>
-      Main
-      <StampBox icon={img} title={title} bgColor={bgColor}></StampBox>
-    </div>
+    <PointBox
+      date={'02-07'}
+      time={'10:59'}
+      name={'토스 뱅크'}
+      point={'10'}
+      transPoint={+10}
+    ></PointBox>
   );
 }
