c27f83bb35d9e18b4159e54573263767b310034e
zoo <anfdnfl@khu.ac.kr>
2025-02-27 15:08:58 +0900

    Feat: Btn 컴포넌트 생성

---
+++ src/components/Btn.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div<{ bgColor: string }>`
+  all: unset;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  padding: 0.5rem 1rem;
+
+  width: fit-content;
+  border-radius: 1rem;
+
+  background-color: ${(props) => props.bgColor};
+`;
+
+interface Props {
+  children: React.ReactNode;
+  bgColor: string;
+  handleBtn: () => void;
+}
+
+export default function Btn({ children, bgColor, handleBtn }: Props) {
+  return (
+    <Wrapper bgColor={bgColor} onClick={handleBtn}>
+      {children}
+    </Wrapper>
+  );
+}
