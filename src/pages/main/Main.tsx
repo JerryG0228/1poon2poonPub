e6b57881e1926faf9da81033ea32a4c3a5176ff6
ooinl77 <ooinl77@naver.com>
2025-02-27 16:25:16 +0900

    Fix: Main.tsx 내용 삭제

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,13 +1,3 @@
-import PointBox from '@/components/PointBox';
-
 export default function Main() {
-  return (
-    <PointBox
-      date={'02-07'}
-      time={'10:59'}
-      name={'토스 뱅크'}
-      point={'10'}
-      transPoint={+10}
-    ></PointBox>
-  );
+  return;
 }
