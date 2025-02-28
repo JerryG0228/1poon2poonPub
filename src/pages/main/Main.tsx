6bc4fac7723543b149768de18b049bf9503ee38f
jhm010234 <jhm010234@naver.com>
2025-02-28 10:56:38 +0900

    Feat: 메인페이지 캐시백 포인트 구현

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -7,16 +7,66 @@ import movieImage from '@/assets/StampBox/MovieIcon.png';
 import storeImage from '@/assets/StampBox/StoreIcon.png';
 import taxiImage from '@/assets/StampBox/TaxiIcon.png';
 import styled from 'styled-components';
+import NonTitleBox from '@/components/NonTitleBox';
+import cashbackImage from '@/assets/CashbackIcon.png';
+import vectorImage from '@/assets/Vector.png';
 
 const MainWrap = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1rem;
 `;
+
 const StampBoxWrap = styled.div`
   display: flex;
   grid-template-columns: repeat(6, 1fr);
   justify-content: space-between;
+`;
+
+const CashBackWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+const CashBackImageWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #6e6430;
+  border-radius: 0.3rem;
+  width: 2rem;
+  height: 2rem;
+  > img {
+    width: 1.2rem;
+    height: 1.25rem;
+  }
+`;
+
+const CashBackTextWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 15rem;
+`;
+
+const Balance = styled.div`
+  font-size: 1.1rem;
+  font-weight: bold;
+`;
+
+const Point = styled.div`
+  font-size: 0.7rem;
+`;
+
+const CashBacArrowWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  > img {
+    justify-content: center;
+    align-items: center;
+    width: 0.8rem;
+    height: 1.1rem;
+  }
 `;
 
 export default function Main() {
@@ -45,6 +95,20 @@ export default function Main() {
             ))}
           </StampBoxWrap>
         </TitleBox>
+        <NonTitleBox>
+          <CashBackWrap>
+            <CashBackImageWrap>
+              <img src={cashbackImage} />{' '}
+            </CashBackImageWrap>
+            <CashBackTextWrap>
+              <Balance>10,000원</Balance>
+              <Point>캐시백 포인트</Point>
+            </CashBackTextWrap>
+            <CashBacArrowWrap>
+              <img src={vectorImage} />
+            </CashBacArrowWrap>
+          </CashBackWrap>
+        </NonTitleBox>
       </MainWrap>
     </>
   );
