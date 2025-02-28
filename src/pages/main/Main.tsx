555c452f2a7714aab89056ab3606d65fe6a97594
ooinl77 <ooinl77@naver.com>
2025-02-28 09:48:24 +0900

    Feat: ETFBox 컴포넌트 추가

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,7 +1,11 @@
+import ETFBox from '@/components/ETFBox';
+
 export default function Main() {
   return (
     <div>
-      <div></div>
+      <div>
+        <ETFBox name={'토스 뱅크 ETF'} price={4000} transPrice={5000} isRecommend={true}></ETFBox>
+      </div>
     </div>
   );
 }
