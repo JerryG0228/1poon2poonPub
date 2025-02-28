a2c94c8c84a2154899d331b88e7be0e3e052b144
jhm010234 <jhm010234@naver.com>
2025-02-28 10:48:14 +0900

    Feat: 메인페이지 캐시백 영역 디자인 구현

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,17 +1,51 @@
-import CategoryBox from '@/components/CategoryBox';
+import StampBox from '@/components/StampBox';
+import TitleBox from '@/components/TitleBox';
+import busImage from '@/assets/StampBox/BusIcon.png';
+import cafeImage from '@/assets/StampBox/CafeIcon.png';
+import foodImage from '@/assets/StampBox/FoodIcon.png';
+import movieImage from '@/assets/StampBox/MovieIcon.png';
+import storeImage from '@/assets/StampBox/StoreIcon.png';
+import taxiImage from '@/assets/StampBox/TaxiIcon.png';
 import styled from 'styled-components';
 
-const Wrap = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
+const MainWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+const StampBoxWrap = styled.div`
+  display: flex;
+  grid-template-columns: repeat(6, 1fr);
+  justify-content: space-between;
 `;
 
 export default function Main() {
+  const stampItem = [
+    { title: '대중교통', img: busImage, bgColor: '#6E5230' },
+    { title: '택시', img: taxiImage, bgColor: '#6E5E3D' },
+    { title: '편의점', img: storeImage, bgColor: '#304E7B' },
+    { title: '영화관', img: movieImage, bgColor: '#683745' },
+    { title: '패스트푸드', img: foodImage, bgColor: '#6E5E3D' },
+    { title: '카페', img: cafeImage, bgColor: '#264038' },
+  ];
+
   return (
-    <Wrap>
-      <CategoryBox title={'교육·문화'}></CategoryBox>
-      <CategoryBox title={'공익·인권'}></CategoryBox>
-      <CategoryBox title={'사회적 가치 & ESG 투자'}></CategoryBox>
-    </Wrap>
+    <>
+      <MainWrap>
+        <TitleBox title="캐시백 영역">
+          <StampBoxWrap>
+            {stampItem.map((item) => (
+              <StampBox
+                key={item.title}
+                icon={item.img}
+                title={item.title}
+                bgColor={item.bgColor}
+                active={true}
+              />
+            ))}
+          </StampBoxWrap>
+        </TitleBox>
+      </MainWrap>
+    </>
   );
 }
