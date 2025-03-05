b2d18fd99c4f67911611d0031f7ae33476d26600
Ham <jhm010234@naver.com>
2025-03-05 09:30:51 +0900

    Rename: 컴포넌트 경로 재수정

--- src/components/Main/PointHistorySection.tsx
+++ src/components/Main/PointHistorySection.tsx
@@ -1,17 +1,7 @@
 import styled from 'styled-components';
-<<<<<<< HEAD
 import NonTitleBox from '@components/NonTitleBox';
-=======
-import NonTitleBox from '@/components/NonTitleBox';
->>>>>>> dev
 import cashbackImage from '@/assets/Main/CashbackIcon.png';
 import vectorImage from '@/assets/Main/Vector.png';
-
-const MainWrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-`;
 
 const CashBackWrap = styled.div`
   display: flex;
