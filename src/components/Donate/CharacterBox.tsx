aed95fb86fa743b8ed5e74c284a7e61b2ca93ef7
zoo <anfdnfl@khu.ac.kr>
2025-03-14 14:50:05 +0900

    Refactor: 사용안하는 코드 삭제

--- src/components/Donate/CharacterBox.tsx
+++ src/components/Donate/CharacterBox.tsx
@@ -1,4 +1,4 @@
-import { motion, useSpring, useTransform } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
 import { colors } from '@/styles/colors';
 import styled, { keyframes, css } from 'styled-components';
